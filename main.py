94d89ae8da58369dcb37bad7a26a798c5bb31682
yuvi01102003 <yuvarajsankar01102003@gmail.com>
2025-08-15 15:45:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from bot import create_app
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port or default 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
